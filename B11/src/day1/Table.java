b8f4fc80dd03fba5eb9a966d2b2e01c23deb248b
Rushikesh <Rushikesh@192.168.0.107>
2022-03-01 15:58:05 +0530

    1st commit

---
+++ B11/src/day1/Table.java
@@ -0,0 +1,18 @@
+package day1;
+
+public class Table {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=2;
+		for(int i=1;i<=10;i++)
+		{
+			
+			System.out.println(num*i);
+			// System.out.printf("%d * %d = %d \n", num, i, num * i);
+			// System.out.printf("%d * %d = %d \r", num, i, num * i);
+		}
+		
+	}
+
+}
