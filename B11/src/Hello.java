e56ce38fefbb6b6fd48cfb5ab94f38a5dac9a71c
Rushikesh <Rushikesh@192.168.0.107>
2022-03-01 15:45:56 +0530

    1st commit

---
+++ B11/src/Hello.java
@@ -0,0 +1,18 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//System.out.format("the %s jumped over the %s, %d times", "cow", "moon", 2);
+		//System.err.format("the %s jumped over the %s, %d times", "cow", "moon", 2);
+		
+		System.out.println("hello world");
+		//System.out.print("hello world");
+		
+		//String result = String.format("the %s jumped over the %s, %d times", "cow", "moon", 2);
+		//System.out.print(result);
+	}
+	
+
+}
